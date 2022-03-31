a3be73ff0054229af9d3bdb8ab0a40d294875014
Zhu Zhanyan <program.nom@gmail.com>
2022-03-31 21:13:44 +0800

    feat: Add Insulin Carb Ratio & Meal time Blood Sugar Averages (#2)
    * chore: gitignore .vim/ from vim editor
    
    * chore: add type comments to ignore type errors flagged by pyright
    
    * feat: add extract_tags() to extract unique tags from sugar_df
    
    * refactor: organise utility functions into feature & excel functions
    
    * feat: add means blood sugar levels by meal to statistics
    
    * build: add black code formatter as dev tooling
    
    * chore: autoformat saccharin.py using black
    
    * feat: add insulin carb ratio feature

--- saccharin.py
+++ saccharin.py
@@ -3,7 +3,7 @@
 # blood sugar data cleaning & export
 #
 
-from typing import cast
+from typing import cast, List
 import numpy as np
 from openpyxl.styles.fills import PatternFill
 from openpyxl.worksheet.table import Table, TableStyleInfo
@@ -16,6 +16,37 @@ from openpyxl.worksheet.worksheet import Worksheet
 from openpyxl.utils.dataframe import dataframe_to_rows
 from openpyxl.formatting.rule import FormulaRule
 from openpyxl.utils import get_column_letter, range_boundaries
+
+
+## Feature Extraction & Engineering
+def to_float(series: pd.Series, default: float) -> pd.Series:
+    """Convert the given series to float, using the given default if conversion fails."""
+    return pd.to_numeric(series, "coerce").fillna(default).astype(float)  # type: ignore
+
+
+def drop_empty(df: pd.DataFrame) -> pd.DataFrame:
+    """Drop fully empty columns from the given DataFrame"""
+    # trim empty whitespace in string columns
+    df = df.applymap((lambda s: s.strip() if isinstance(s, str) else s))
+
+    # remove empty & nan coluns
+    df = df.replace("", np.nan).dropna(axis=1, how="all")  # type: ignore
+
+    return df
+
+
+def sort_tags(tags: pd.Series) -> pd.Series:
+    """Sort the comma seperated tags in the given series"""
+    return (
+        tags.str.split(",")
+        .map(
+            # list type check needed as sorted cannot sort discrete float values
+            lambda tags: tags
+            if not isinstance(tags, List)
+            else sorted(tags)
+        )
+        .str.join(",")
+    )
 
 
 def read_sugar_df(csv_path: str) -> pd.DataFrame:
@@ -44,26 +75,28 @@ def read_sugar_df(csv_path: str) -> pd.DataFrame:
     # parse date & time columns
     sugar_df["Date"] = pd.to_datetime(sugar_df["Date"]).apply((lambda dt: dt.date()))  # type: ignore
     sugar_df["Time"] = pd.to_datetime(sugar_df["Time"]).apply((lambda dt: dt.time()))  # type: ignore
-    return sugar_df  # type: ignore
+    # ensure identical sets of tags are represented by the same string
+    sugar_df["Tags"] = sort_tags(sugar_df["Tags"])
+    return drop_empty(sugar_df)
 
 
-def drop_empty(df: pd.DataFrame) -> pd.DataFrame:
-    """Drop fully empty columns from the given DataFrame"""
-    # trim empty whitespace in string columns
-    df = df.applymap((lambda s: s.strip() if isinstance(s, str) else s))
+def label_outlier(sugar_level: float, outlier_high: float, outlier_low: float) -> str:
+    """Label if the given sugar level is an outlier: outside outlier high & low constraints."""
+    if sugar_level > outlier_high:
+        return "High"
+    elif sugar_level < outlier_low:
+        return "Low"
+    else:
+        return ""
 
-    # remove empty & nan coluns
-    df = df.replace("", np.nan).dropna(axis=1, how="all")  # type: ignore
 
-    return df
-
-
+## Excel Utilities
 def fit_sheet_cols(worksheet: Worksheet):
     """Autofit the given worksheet's columns to content"""
     for col in worksheet.iter_cols():
         col_letter = col[0].column_letter
         col_width = max([len(str(cell.value)) for cell in col])
-        worksheet.column_dimensions[col_letter].width = col_width * 1.05
+        worksheet.column_dimensions[col_letter].width = col_width * 1.05  # type: ignore
     return worksheet
 
 
@@ -83,14 +116,6 @@ def convert_table(worksheet: Worksheet) -> Table:
 
     return table
 
-def label_outlier(sugar_level: float, outlier_high: float, outlier_low: float) -> str:
-    """Label if the given sugar level is an outlier: outside outlier high & low constraints. """
-    if sugar_level > outlier_high:
-        return "High"
-    elif sugar_level < outlier_low:
-        return "Low"
-    else:
-        return ""
 
 def fill_conditional(
     worksheet: Worksheet, address: str, condition: str, color_hex: str
@@ -99,7 +124,7 @@ def fill_conditional(
     # remove hash sign in color_hex if present
     color = color_hex.replace("#", "")
 
-    worksheet.conditional_formatting.add(
+    worksheet.conditional_formatting.add(  # type: ignore
         address,
         FormulaRule(
             formula=[condition],
@@ -197,7 +222,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # read blood sugar data
-    sugar_df = drop_empty(read_sugar_df(args.csv_path))
+    sugar_df = read_sugar_df(args.csv_path)
 
     # filter data to only include entries from start date onwards
     if args.start_from is not None:
@@ -207,11 +232,24 @@ if __name__ == "__main__":
     sugar_df["Hyperglycemia"] = sugar_df["Blood Sugar Measurement (mmol/L)"] > 10.0
     sugar_df["Hypoglycemia"] = sugar_df["Blood Sugar Measurement (mmol/L)"] < 4.0
 
+    # add insulin carb ratio (ICR) features
+    sugar_df["Total Insulin (Meal)"] = to_float(
+        sugar_df["Insulin (Meal)"], default=0.0
+    ) + to_float(sugar_df["Insulin (Correction)"], default=0.0)
+
+    sugar_df["Insulin Carb Ratio (ICR)"] = (
+        sugar_df["Meal Carbohydrates (Grams, Factor 1)"]
+        / sugar_df["Total Insulin (Meal)"]
+    )
+
     # add outlier features
     sugar_df["Outlier"] = [
         label_outlier(
-            sugar, args.outlier_high, args.outlier_low,
-        ) for sugar in sugar_df["Blood Sugar Measurement (mmol/L)"]
+            sugar,
+            args.outlier_high,
+            args.outlier_low,
+        )
+        for sugar in sugar_df["Blood Sugar Measurement (mmol/L)"]
     ]
 
     # compute summary statistics
@@ -225,6 +263,25 @@ if __name__ == "__main__":
         glycemia = f"{prefix}glycemia"
         count = sugar_df[sugar_df[glycemia]][glycemia].agg("count")
         stats_df.loc["count", glycemia] = count
+
+    # compute average blood sugar level by meal
+    meal_tags = ["Breakfast", "Lunch", "Dinner", "Snack"]
+
+    # filter blood sugar measurements with no tags (na = False)
+    meal_sugar_df = sugar_df[
+        sugar_df["Tags"].str.contains("|".join(meal_tags), na=False)
+    ]
+    meal_stats_df = (
+        meal_sugar_df[["Blood Sugar Measurement (mmol/L)", "Tags"]]
+        .groupby(by="Tags")
+        .agg("mean")
+        .rename(
+            # add suffix to clarify that its blood sugar
+            index=lambda s: f"{s} (mmol/L)",
+            columns={"Blood Sugar Measurement (mmol/L)": "mean"},
+        )
+    )
+    stats_df = stats_df.join(meal_stats_df.T)
 
     # template and save as excel file
     workbook = template_excel(sugar_df, stats_df)
